97939610ce1b23856585dcddbcf109d14c0b3612
Johan Fabi <johanfabi@outlook.com>
2024-06-24 23:48:16 -0400

    Update: Jenkinsfile

--- aws/jenkins/lambda-function/main.py
+++ aws/jenkins/lambda-function/main.py
@@ -1,14 +1,10 @@
 # AWS Python Lambda Function Hello World Example
-import random
+import json
 
-def mezclar_nombre(nombre):
-    """Mezcla aleatoriamente las letras de un nombre."""
-    letras = list(nombre)  # Convierte el nombre en una lista de letras
-    random.shuffle(letras)  # Reordena la lista de forma aleatoria
-    nombre_mezclado = "".join(letras)  # Une las letras en un nuevo string
-    return nombre_mezclado
-
-# Ejemplo de uso:
-nombre_original = input("Ingresa un nombre: ")
-nombre_mezclado = mezclar_nombre(nombre_original)
-print("Nombre mezclado:", nombre_mezclado)
+def lambda_handler(event, context):
+    # Log the event argument for debugging and for in-depth troubleshooting
+    print("Received event: " + json.dumps(event, indent=2))
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
