5735f8afbde135216da692feedd8c76b409f1cb2
Johan Fabi <johanfabi@outlook.com>
2024-06-25 00:10:01 -0400

    Update: Code

--- aws/jenkins/lambda-function/main.py
+++ aws/jenkins/lambda-function/main.py
@@ -1,16 +1,10 @@
 # AWS Python Lambda Function Hello World Example
 import json
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-MESSAGE = os.getenv("MESSAGE")
 
 def lambda_handler(event, context):
     # Log the event argument for debugging and for in-depth troubleshooting
     print("Received event: " + json.dumps(event, indent=2))
     return {
         'statusCode': 200,
-        'body': json.dumps(MESSAGE)
+        'body': json.dumps('Hello World From Lambda!!!')
     }
